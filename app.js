1f26b8bdea039eff03fb91cbc9c9099898536b3d
Bolupe Taiwo <105107337+Bolupe@users.noreply.github.com>
2022-08-31 09:04:14 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //  Function for conversions
+//Feet 2 Inches
 const feetInput = document.getElementById('feet')
 const inchesInput = document.getElementById('inches')
  
@@ -25,56 +26,38 @@ feetInput.addEventListener('input', () => {
    feetInput.value = Math.round(convertedValue);
 })
 
-
-function kiloweightConvert(value) {
-    document.getElementById("Pounds").innerHTML=value*2.2046;
-}
-
-
-
+//BMI Calculation
 window.onload = () => {
     let button = document.querySelector("#btn");
-  
-   
     button.addEventListener("click", calculateBMI);
 };
-
-
-
-
-// Function for calculating BMI
+  
 function calculateBMI() {
     let height = parseInt(document
-            .querySelector("#height").value);
-  
-    let weight = parseInt(document
+            .querySelector("#inches").value);
+    let weightOld = parseInt(document
             .querySelector("#weight").value);
-  
+    let weight = weightOld*2.2046;
     let result = document.querySelector("#result");
   
-    
+    // Checking the user providing a proper value or not
     if (height === "" || isNaN(height)) 
         result.innerHTML = "Provide a valid Height!";
   
     else if (weight === "" || isNaN(weight)) 
         result.innerHTML = "Provide a valid Weight!";
   
-    
+    // If both input is valid, calculate the bmi
     else {
-  
-      
+        // Fixing upto 2 decimal places
         let bmi = 703 * (weight / (height * height)).toFixed(2);
   
-       
-        if (bmi < 18.6) 
-            result.innerHTML =
-            "Under Weight : " + bmi;
-
-        else if (bmi >= 18.6 && bmi < 24.9) 
-            result.innerHTML = 
-            "Normal : " + bmi;
+        // Dividing as per the bmi conditions
+        if (bmi < 18.6) result.innerHTML = "Under Weight : " + bmi;
   
-        else result.innerHTML =
-             "Over Weight : " + bmi;
+        else if (bmi >= 18.6 && bmi < 24.9) 
+            result.innerHTML = "Normal : " + bmi;
+  
+        else result.innerHTML = "Over Weight : " + bmi;
     }
 }
